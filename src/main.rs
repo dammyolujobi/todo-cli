b9833362d5d40cb17ace55fbbc60f8f1d956d725
dammyjones <daolabmovies@gmail.com>
2026-02-22 02:48:51 +0100

    Beautified Table

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use std::io;
 
 fn main() {
+    let mut store: HashMap<String, String> = HashMap::new();
+
     println!("Enter value to notes or quit to close the program");
 
     loop {
@@ -11,22 +13,29 @@ fn main() {
             .read_line(&mut input_text)
             .expect("Failed to read message");
 
-        if input_text.trim().to_string().to_lowercase() != "quit" {
-            add_new_entry(input_text.trim().to_string());
-        } else if input_text.trim().to_string().to_lowercase() == "quit" {
+        let new_text = input_text.trim().to_string();
+
+        if new_text.to_lowercase() != "quit" {
+            add_new_entry(new_text, &mut store);
+        } else if new_text.to_lowercase() == "quit" {
+            println!("\n");
             break;
         }
     }
+    get_todo(&mut store);
 }
 
-fn add_new_entry(data: String) {
-    let mut map: HashMap<String, String> = HashMap::new();
+fn add_new_entry(data: String, store: &mut HashMap<String, String>) {
     let utc_now = Utc::now();
 
-    let utc_now_string = utc_now.time().to_string();
-    map.insert(data, utc_now_string);
+    let utc_now_string = utc_now.format("%H:%M:%S %p").to_string();
+    let _ = &store.insert(data, utc_now_string);
+}
 
-    for (dt, time) in &map {
-        println!("{} {} ", dt, time);
+fn get_todo(store: &mut HashMap<String, String>) {
+    println!("{:<20} {:<20}", "Note", "Time");
+    println!("{:-<40}", "");
+    for (key, value) in store.iter() {
+        println!("{:<20} {:<20}", key, value);
     }
 }
